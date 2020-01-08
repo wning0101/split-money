2343dbfcc2ac469f367361a42b11b46a02e40302
wning0101 <willylin0607.com>
2020-01-07 18:40:23 -0600

    settle function 1.0

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ class Note extends React.Component {
         number: 0,
         total_spend: 0,
         members: [],
-        result: "qweqw",
+        result: [],
         record : [],
         each_spend : {},
-        each_share : {}
+        each_share : {},
     }
     
 
@@ -93,15 +93,81 @@ class Note extends React.Component {
             each_spend : cur_spend,
             record : cur_record
         })
-        document.getElementById("record").innerHTML = "spending record: "
+        document.getElementById("record").innerHTML = "Spending record: "
         for(i=0;i<this.state.record.length;i++){
             document.getElementById("record").innerHTML += this.state.record[i];
+        }
+        this.settle();
+        document.getElementById("result").innerHTML = "Final settle: <br>"
+        for(i=0;i<this.state.result.length;i++){
+            document.getElementById("result").innerHTML += "<li>" + this.state.result[i] + "</li>";
         }
 
         return;
     }
 
     settle() {
+        var pos = [];
+        var neg = [];
+        var equ = [];
+        var record = [];
+        var i = 0;
+        for(i=0; i < this.state.members.length;i++){
+            var name = this.state.members[i];
+            var balance = this.state.each_spend[name] - this.state.each_share[name];
+            if (balance>0){
+                pos.push([balance, name]);
+            }
+            else if (balance<0){
+                neg.push([balance, name]);
+            }
+            else{
+                equ.push([0, name]);
+            }
+        }
+        function compare(a, b) {
+            if (a[0] < b[0]) return -1;
+            if (a[0] > b[0]) return 1;
+            return 0;
+        }
+        function compare2(a, b) {
+            if (a[0] < b[0]) return 1;
+            if (a[0] > b[0]) return -1;
+            return 0;
+        }
+        pos = pos.sort(compare);
+        neg = neg.sort(compare2);
+        var temp = 0;
+        while(pos.length > 0 && neg.length > 0 ){
+            var transfer = pos[0][0] + neg[0][0];
+            if (transfer>0){
+                record.push(neg[0][1] + " gives " + pos[0][1] + " " + String(neg[0][0]) + " dollars.");
+                pos[0][0] += neg[0][0];
+                temp = pos[0][0];
+                pos.shift();
+                neg.shift();
+                pos.push(temp);
+                pos = pos.sort(compare)
+            }
+            else if(transfer<0){
+                record.push(neg[0][1] + " gives " + pos[0][1] + " " + String(pos[0][0]) + " dollars.");
+                neg[0][0] += pos[0][0];
+                temp = neg[0][0];
+                pos.shift();
+                neg.shift();
+                neg.push(temp);
+                neg = neg.sort(compare2);
+            }
+            else{
+                record.push(neg[0][1] + " gives " + pos[0][1] + " " + String(pos[0][0]) + " dollars.");
+                pos.shift();
+                neg.shift();
+            }
+        }
+        this.setState({
+            result: record,
+        })
+
         return;
     }
     
@@ -134,7 +200,7 @@ class Note extends React.Component {
         <br/>
         <div id="member"> Member: {this.state.members} </div>
         <div id="record"> spending record: {this.state.record} </div>
-        <div> Final settle: {this.state.result}</div>
+        <div id="result"> Final settle: {this.state.result}</div>
         </>
         );
     }
