edc95230e1bd65c7973627b188758953b046892b
wning0101 <willylin0607.com>
2020-06-17 17:02:27 +0800

    build

---
+++ build/precache-manifest.841e7ffc339bcc682d3ff32e4a96c00d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a49ba356818d0685514bd33ed95c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbb9a79a5854cc1a2ea0",
+    "url": "/static/css/main.b333f655.chunk.css"
+  },
+  {
+    "revision": "d7572048dd3cb86717e3",
+    "url": "/static/js/2.ebb18b38.chunk.js"
+  },
+  {
+    "revision": "d05bc1e454a73e96574c9feb8b334eab",
+    "url": "/static/js/2.ebb18b38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbb9a79a5854cc1a2ea0",
+    "url": "/static/js/main.8d945486.chunk.js"
+  },
+  {
+    "revision": "fa45db5505c3008ae1f3",
+    "url": "/static/js/runtime-main.8d7f195d.js"
+  }
+]);
